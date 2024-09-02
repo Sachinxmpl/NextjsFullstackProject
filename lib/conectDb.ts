e04802767052a9c188fdb442b3efff182446cd83
Sachinxmpl <sachinxmpl6@gmail.com>
2024-09-02 23:28:15 +0545

    add zod , db

--- lib/conectDb.ts
+++ lib/conectDb.ts
@@ -1,4 +1,23 @@
 import mongoose from "mongoose"
 
-type ConnectionOject  = {
+type ConnectionObject  = {
+    isConnected? : number 
+}
+
+const connection : ConnectionObject = {} 
+
+const connectDb = async() : Promise<void> =>{
+     if(connection.isConnected){
+        console.log("Already connected to database")
+        return
+     }
+
+     try{
+        const db = await mongoose.connect(process.env.DB_URI || "")
+        connection.isConnected = db.connections[0].readyState
+     }
+     catch(e){
+        console.log(e)
+        process.exit()
+     }
 }
